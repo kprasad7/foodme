3fb760701e49689573b16eb787802102b359eb25
kprasad7 <kanuridurgaprasad049@gmail.com>
2023-11-25 18:40:28 +0530

    changes

--- foodme/settings.py
+++ foodme/settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+
+import django_heroku
+#django_heroku.settings(locals())
+
 
 from pathlib import Path
 
@@ -25,7 +30,7 @@ SECRET_KEY = 'django-insecure--5rln(gy_!w)j)$z!(@e=ua!$v$1vjjm6kpm5vy*7#xxnvi^=a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
